4429586a8f73ca7ba7dbc2ee2fb4f896ed13b60e
Jaime <jaime.valdez@potros.itson.edu.mx>
2023-03-23 02:17:11 -0700

    tercer commit customCircle y customBar agregados

--- app/src/main/java/valdez/jaime/myfeelings/utilities/CustomCircleDrawable.kt
+++ app/src/main/java/valdez/jaime/myfeelings/utilities/CustomCircleDrawable.kt
@@ -1,24 +1,72 @@
 package valdez.jaime.myfeelings.utilities
 
+import android.content.Context
 import android.graphics.Canvas
 import android.graphics.ColorFilter
+import android.graphics.Paint
+import android.graphics.PixelFormat
+import android.graphics.RectF
 import android.graphics.drawable.Drawable
+import androidx.core.content.ContextCompat
+import valdez.jaime.myfeelings.R
 
-class CustomCircleDrawable: Drawable() {
-    
+class CustomCircleDrawable: Drawable {
+    var coordenadas: RectF? = null
+    var anguloBarrido: Float = 0.0F
+    var anguloinicio: Float = 0.0F
+    var grosorMetrica: Int = 0
+    var grosorFondo: Int = 0
+    var context: Context? = null
+    var emociones = ArrayList<Emociones>()
+    constructor(context: Context, emociones: ArrayList<Emociones>){
+        this.context = context
+        grosorMetrica = context.resources.getDimensionPixelSize(R.dimen.graphWith)
+        grosorFondo = context.resources.getDimensionPixelSize(R.dimen.graphBackground)
+        this.emociones = emociones
+    }
+
     override fun draw(p0: Canvas) {
-        TODO("Not yet implemented")
+        val fondo: Paint =  Paint()
+        fondo.style = Paint.Style.STROKE
+        fondo.strokeWidth = (this.grosorFondo).toFloat()
+        fondo.isAntiAlias = true
+        fondo.strokeCap = Paint.Cap.ROUND
+        fondo.color = context?.resources?.getColor(R.color.gray) ?: R.color.gray
+        val ancho: Float = (p0.width - 25).toFloat()
+        val alto: Float = (p0.height - 25).toFloat()
+
+        coordenadas = RectF(25.0F, 25.0F, ancho, alto)
+
+        p0.drawArc(coordenadas!!, 0.0F, 360.0F, false, fondo)
+
+        if (emociones.size != 0){
+            for (e in emociones){
+                val degree: Float = (e.porcentaje*360)/100
+                this.anguloBarrido = degree
+
+                var seccion: Paint = Paint()
+                seccion.style = Paint.Style.STROKE
+                seccion.strokeWidth = (this.grosorMetrica).toFloat()
+                seccion.isAntiAlias = true
+                seccion.strokeCap = Paint.Cap.SQUARE
+                seccion.color = ContextCompat.getColor(this.context!!, e.color)
+
+                p0.drawArc(coordenadas!!, this.anguloinicio, this.anguloBarrido, false, seccion)
+                this.anguloinicio += this.anguloBarrido
+            }
+        }
+
     }
 
     override fun setAlpha(p0: Int) {
-        TODO("Not yet implemented")
+
     }
 
     override fun setColorFilter(p0: ColorFilter?) {
-        TODO("Not yet implemented")
+
     }
 
     override fun getOpacity(): Int {
-        TODO("Not yet implemented")
+        return PixelFormat.OPAQUE
     }
 }

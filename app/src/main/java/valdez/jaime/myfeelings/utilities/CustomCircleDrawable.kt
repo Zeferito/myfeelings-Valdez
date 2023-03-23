e884e78bb75dca86fd2ed13434c43d085c7a6daf
Jaime <jaime.valdez@potros.itson.edu.mx>
2023-03-22 22:41:15 -0700

    second commit package utilities

---
+++ app/src/main/java/valdez/jaime/myfeelings/utilities/CustomCircleDrawable.kt
@@ -0,0 +1,24 @@
+package valdez.jaime.myfeelings.utilities
+
+import android.graphics.Canvas
+import android.graphics.ColorFilter
+import android.graphics.drawable.Drawable
+
+class CustomCircleDrawable: Drawable() {
+    
+    override fun draw(p0: Canvas) {
+        TODO("Not yet implemented")
+    }
+
+    override fun setAlpha(p0: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun setColorFilter(p0: ColorFilter?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getOpacity(): Int {
+        TODO("Not yet implemented")
+    }
+}
